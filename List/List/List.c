504f98f56524ff145fc469bb9ec97ef2b6ad2c88
KomarDL <deagl_Dim@mail.ru>
2019-12-15 00:00:31 +0300

    Add: implement list constructor

--- List/List/List.c
+++ List/List/List.c
@@ -19,3 +19,27 @@ typedef struct _TList
 	DataDestructor dataDestructor;
 	DataCopy dataCopy;
 } List;
+
+PList ListConstructor(DataConstructor dataConstructor, DataDestructor dataDestructor,
+					GetParams getParams, ReleaseParams releaseParams,
+					DataCopy dataCopy, int dataSize)
+{
+	if (getParams == NULL || releaseParams == NULL)
+	{
+		SetLastError(ERROR_INVALID_PARAMETER);
+		return NULL;
+	}
+
+	PList result = calloc(1, sizeof(List));
+	if (result != NULL)
+	{
+		result->isEmpty = true;
+		result->dataSize = dataSize;
+		result->dataConstructor = (dataConstructor == NULL ? malloc : dataConstructor);
+		result->dataDestructor = (dataDestructor == NULL ? free : dataDestructor);
+		result->dataCopy = (dataCopy == NULL ? memcpy_s : dataCopy);
+		result->getParams = getParams;
+		result->releaseParams = releaseParams;
+	}
+	return result;
+}
--- List/List/List.h
+++ List/List/List.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <memory.h>
+#include <WinError.h>
+#include <errhandlingapi.h>
 
 typedef void(*OutputFunction)(void *data);
 typedef bool(*Comparator)(void *first, void *second);
@@ -12,6 +14,12 @@ typedef void(*ReleaseParams)(void *params);
 typedef void*(*DataConstructor)(void *params);
 typedef void(*DataDestructor)(void *data);
 typedef errno_t(*DataCopy)(void *dest, size_t destSize, const void *src, size_t count);
+
+typedef struct _TList *PList;
+
+PList ListConstructor(DataConstructor dataConstructor, DataDestructor dataDestructor,
+					GetParams getParams, ReleaseParams releaseParams,
+					DataCopy dataCopy, int dataSize);
 
 #endif // !UNIVERSAL_LIST
 
