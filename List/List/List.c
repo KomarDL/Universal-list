ca8ff543a858feed3495a0a5063fc38ad63f5e2f
KomarDL <deagl_Dim@mail.ru>
2019-12-14 23:36:24 +0300

    Add: define List type

--- List/List/List.c
+++ List/List/List.c
@@ -5,3 +5,17 @@ typedef struct _TNode
 	void* data;
 	struct _TNode *next;
 } Node, *PNode;
+
+typedef struct _TList
+{
+	PNode node;
+	PNode lastNode;
+	int length;
+	bool isEmpty;
+	int dataSize;
+	GetParams getParams;
+	ReleaseParams releaseParams;
+	DataConstructor dataConstructor;
+	DataDestructor dataDestructor;
+	DataCopy dataCopy;
+} List;
